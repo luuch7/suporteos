3eb399d2304969dabceba1a67a8cab7d32fa69cb
luuch7 <camargoguilhermed99@gmail.com>
2024-03-04 21:05:08 -0300

    suporteos3

---
+++ src/main/java/com/curso/suporteos/SuporteosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.suporteos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuporteosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuporteosApplication.class, args);
+	}
+
+}
